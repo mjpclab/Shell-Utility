aba4f37fb5a67ee90ee28054dae4eaa4baf9e72c
marjune <marjune@163.com>
2020-03-05 11:24:41 +0800

    feat: update git-branch-ps1

--- profile.d/git-branch-ps1.sh
+++ profile.d/git-branch-ps1.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
 getGitCurrentBranch() {
-	branch=$(git rev-parse --abbrev-ref HEAD 2> /dev/null);
+	local branch=$(git rev-parse --abbrev-ref HEAD 2> /dev/null);
 	if [ -n "$branch" ]; then
-		result="($branch)";
+		result=" ($branch)";
 	fi
 	
 	echo "$result"
 }
 
-PS1='\n[\u@\h \w] $(getGitCurrentBranch)\n\$ '
-
+PS1='\n[\u@\h \w]$(getGitCurrentBranch)\n\$ '
